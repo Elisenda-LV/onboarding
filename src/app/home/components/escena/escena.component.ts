759c526453a4ef7da41d4bda9f42e28318adc8ba
Elisenda-LV <elisenda.lv@gmail.com>
2023-11-22 18:02:08 +0100

    exercici 5.1

--- src/app/home/components/escena/escena.component.ts
+++ src/app/home/components/escena/escena.component.ts
@@ -1,17 +1,52 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Query } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from '../../pages/main-page/main-page.component';
 
-import { StepsService } from '../../services/steps.service';
 import { Istep } from '../../interfaces/istep.interface';
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
+
+/* const left = [
+  query(':enter, :leave', style({position: 'fixed', widht:'100%'}), {
+    optional: true }),
+
+  group([
+    query('enter', [style({ transform: 'translateX(-100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))],{
+      optional: true,
+    }),
+    query(':leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(100%)' }))], {
+      optional: true,
+    }),
+  ]),
+];
+
+const right = [
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+    optional: true }),
+  group([
+    query(':enter', [style({ transform: 'translateX(100%)' }), animate('.3s ease-out', style({ transform:'translateX(0%)' }))], {
+      optional: true,
+    }),
+    query('.leave', [style({ transform: 'translateX(0%)' }), animate('.3s ease-out', style({ transform: 'translateX(-100%)' }))], {
+      optional: true,
+    }),
+  ]),
+]; */
 
 @Component({
   selector: 'home-escena',
   standalone: true,
   imports: [CommonModule, MainPageComponent],
   templateUrl: './escena.component.html',
-  styleUrl: './escena.component.css'
+  styleUrl: './escena.component.css',
+ /*  animations: [
+    trigger('animStep', [
+      transition(':increment', right),
+      transition(':decrement', left),
+    ])
+  ]*/
+
+
 })
 
 
@@ -25,7 +60,6 @@ public stepsArray: Istep [] = [{
   img:'',
   bgcolor:''
 }
-
 
 ]
 
